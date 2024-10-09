727de03e3104fa39f080c11a1e8becde380581b5
abhishek <saikrishnaabhishek2@gmail.com>
2024-10-09 23:03:25 +0530

    Addition of two Numbers

---
+++ Testing/Addition.java
@@ -0,0 +1,24 @@
+package Testing;
+
+public class Addition {
+    int firstNumber;
+    int secondNumber;
+
+    public Addition(int firstNumber, int secondNumber) {
+        this.firstNumber = firstNumber;
+        this.secondNumber = secondNumber;
+    }
+    int add()
+    {
+        System.out.println("The addition of two numbers is");
+        System.out.println(firstNumber+secondNumber);
+        return firstNumber+secondNumber;
+    }
+
+    public static void main(String args[])
+    {
+        Addition addition = new Addition(5,10);
+        addition.add();
+    }
+}
+
