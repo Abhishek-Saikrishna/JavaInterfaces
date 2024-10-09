41570514e1b17241c92535dfa8f2d0d86be9c743
abhishek <saikrishnaabhishek2@gmail.com>
2024-10-09 21:45:30 +0530

    modified Toyoto.java

--- interfaces/Toyoto.java
+++ interfaces/Toyoto.java
@@ -3,6 +3,7 @@ package interfaces;
 public class Toyoto implements Car{
     @Override
     public void drive() {
+        System.out.println("You are currently in branch1");
         System.out.println("This is on branch1 Toyoto Car");
     }
 }
