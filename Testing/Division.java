d2e8d008a67dd62305b112922861dfe71a937186
abhishek <saikrishnaabhishek2@gmail.com>
2024-10-10 00:19:50 +0530

    Division File added

---
+++ Testing/Division.java
@@ -0,0 +1,13 @@
+package Testing;
+
+public class Division {
+    void divide()
+    {
+        System.out.println("This is Division");
+    }
+    public static void main(String args [])
+    {
+        Division division = new Division();
+        division.divide();
+    }
+}
